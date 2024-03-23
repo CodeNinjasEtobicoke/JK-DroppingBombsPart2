68e195210fe6e310c7ffb197094f0545adf64c12
I Love Iced Coffee <107214218+CodeNinjasEtobicoke@users.noreply.github.com>
2024-03-23 11:18:14 -0400

    joshia

---
+++ Assets/Scripts/RocketAnimate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketAnimate : MonoBehaviour
+{
+    [Header("Animation")]
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        animator.SetFloat("HAxis", horizontal);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
