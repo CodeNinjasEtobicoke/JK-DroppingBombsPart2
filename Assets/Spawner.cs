af898d22eb40a770e20a93874721fa77e2c13891
I Love Iced Coffee <107214218+CodeNinjasEtobicoke@users.noreply.github.com>
2024-04-03 18:54:41 -0400

    joshua

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -8,10 +8,25 @@ public class Spawner : MonoBehaviour
     public float delay = 2.0f;
     public bool active = true;
     public Vector2 delayRange = new Vector2(1, 2);
+
+    private Vector2 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ResetDelay();
+        StartCoroutine(EnemyGenerator());
+    }
+    IEnumerator EnemyGenerator()
+    {
+        yield return new WaitForSeconds(delay);
+        if(active)
+        {
+            Instantiate(bombPrefab, new Vector3(randomX, spawnY, 0), bombPrefab.transform.rotation);
+            ResetDelay();
+        }
+        StartCoroutine(EnemyGenerator());
     }
 
     // Update is called once per frame
@@ -19,7 +34,11 @@ public class Spawner : MonoBehaviour
     {
         
     }
-    
+    void ResetDelay()
+    {
+        delay = Random.Range(delayRange.x, delayRange.y);
+    }
+
 
 
 }
