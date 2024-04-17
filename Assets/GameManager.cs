0e18b8e31909336836feef13d04d982e811c3083
I Love Iced Coffee <107214218+CodeNinjasEtobicoke@users.noreply.github.com>
2024-04-17 19:07:13 -0400

    joshua

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,10 +7,16 @@ public class GameManager : MonoBehaviour
     private Spawner spawner;
     public GameObject title;
     private Vector2 screenBounds;
-     void Awake()
+    public GameObject playerPrefab;
+    private GameObject player;
+    private bool gameStarted = false;
+    public GameObject splash;
+
+    void Awake()
     {
         spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
-        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z))
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        player = playerPrefab;
     }
     // Start is called before the first frame update
     void Start()
@@ -22,15 +28,36 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKeyDown)
+        if (!gameStarted)
         {
-            spawner.active = true;
-            title.SetActive(false);
-        }
-        var nextBomb = gameObject.FindGameObjectsWithTag("Bomb");
-        foreach (GameObject bombObject in nextBomb)
-        {
-            if 
+
+            if (Input.anyKeyDown)
+            {
+                ResetGame();
+            }else
+            {
+                if (!player)
+                {
+                    OnPlayerKilled();
+                }
+            }        
         }
     }
+    private void ResetGame()
+    {
+        spawner.active = true;
+        title.SetActive(false);
+        player = Instantiate(playerPrefab, new Vector3(0, 0, 0), playerPrefab.transform.rotation);
+        gameStarted = true;
+    }
+    void OnPlayerKilled()
+    {
+        spawner.active = false;
+        gameStarted = false;
+
+
+        splash.SetActive(true);
+    }
 }
+
+
